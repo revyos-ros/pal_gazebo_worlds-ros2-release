b8ba714801ae94d261a087ec7d7cb63920356869
Noel Jimenez <noel.jimenez@pal-robotics.com>
2022-10-20 13:15:51 +0200

    Imported upstream version '3.0.3' of 'upstream'

--- launch/pal_gazebo.launch.py
+++ launch/pal_gazebo.launch.py
@@ -1,39 +1,25 @@
-# Copyright (c) 2020 PAL Robotics S.L. All rights reserved.
+# Copyright (c) 2022 PAL Robotics S.L. All rights reserved.
 #
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#  1. Redistributions of source code must retain the above copyright notice,
-#  this list of conditions and the following disclaimer.
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#  3. Neither the name of the copyright holder nor the names of its
-#  contributors may be used to endorse or promote products derived from this
-#  software without specific prior written permission.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-#  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-#  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-#  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 from os import environ, pathsep
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, SetEnvironmentVariable
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -41,15 +27,15 @@ def generate_launch_description():
     try:
         priv_pkg_path = get_package_share_directory(
             'pal_gazebo_worlds_private')
-        model_path = os.path.join(priv_pkg_path, "models") + pathsep
+        model_path = os.path.join(priv_pkg_path, 'models') + pathsep
         resource_path = priv_pkg_path + pathsep
-    except:
-        model_path = ""
-        resource_path = ""
+    except Exception:
+        model_path = ''
+        resource_path = ''
 
     # Add pal_gazebo_worlds path
     pkg_path = get_package_share_directory('pal_gazebo_worlds')
-    model_path += os.path.join(pkg_path, "models")
+    model_path += os.path.join(pkg_path, 'models')
     resource_path += pkg_path
 
     if 'GAZEBO_MODEL_PATH' in environ:
@@ -59,23 +45,31 @@ def generate_launch_description():
 
     declare_world_name = DeclareLaunchArgument(
             'world_name', default_value='',
-            description='Specify world name, we\'ll convert to full path'
+            description="Specify world name, we'll convert to full path"
         )
 
     world_path = PathJoinSubstitution(
-            substitutions=[pkg_path, "worlds",
-                           PythonExpression(['"', LaunchConfiguration("world_name"), '.world"'])])
+            substitutions=[pkg_path, 'worlds',
+                           PythonExpression(['"', LaunchConfiguration('world_name'), '.world"'])])
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        launch_arguments={'world': world_path}.items(),
-    )
+    start_gazebo_server_cmd = ExecuteProcess(
+        cmd=['gzserver', '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so', world_path],
+        output='screen')
 
-    return LaunchDescription([
-        declare_world_name,
-        SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path),
-        # Using this prevents shared library from being found
-        # SetEnvironmentVariable("GAZEBO_RESOURCE_PATH", resource_path),
-        gazebo,
-    ])
+    start_gazebo_client_cmd = ExecuteProcess(
+        cmd=['gzclient'], output='screen')
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(declare_world_name)
+
+    ld.add_action(SetEnvironmentVariable('GAZEBO_MODEL_PATH', model_path))
+    # Using this prevents shared library from being found
+    # ld.add_action(SetEnvironmentVariable('GAZEBO_RESOURCE_PATH', resource_path))
+
+    ld.add_action(start_gazebo_server_cmd)
+    ld.add_action(start_gazebo_client_cmd)
+
+    return ld
